0a98ae453598a1ccd22df2a86fd6dea753566901
Lin, Ming Chih <mingchih.lin8@gmail.com>
2021-10-22 19:52:37 +0800

    Add delete after days

--- business.py
+++ business.py
@@ -1,7 +1,9 @@
 queue_dir = 'c:/demo/'
+days_to_keep = 1
 
 import os
 import time
+import datetime
 import shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -29,7 +31,16 @@ class MyHandler(FileSystemEventHandler):
             shutil.make_archive(dir_name, 'zip', dir_name)
             os.chdir(queue_dir)
             shutil.rmtree(dir_name)
-        
+
+def deleteFilesOverDays():
+    for dirpath, dirnames, filenames in os.walk(queue_dir):
+       for file in filenames:
+          curpath = os.path.join(dirpath, file)
+          file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(curpath))
+          if datetime.datetime.now() - file_modified > datetime.timedelta(days=days_to_keep):
+              os.remove(curpath)
+
+
 if __name__ == "__main__":
     event_handler = MyHandler()
     observer = Observer()
@@ -51,7 +62,10 @@ if __name__ == "__main__":
                     os.chdir(new_folder)
                     os.system(f'ansysedt -BatchSolve -ng -monitor -autoextract reports -machinelist list="localhost:4:20:90%:1" {new_aedt_path}')
                 except:
-                    pass
+                    time.sleep(10)
+
+            deleteFilesOverDays()
+                
     except KeyboardInterrupt:
         observer.stop()
 
